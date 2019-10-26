fe570f49764bbc956dd41ada700ea352abc7d8cf
dengshuaihui <dengshuaihui@yy.com>
2019-10-26 14:47:54 +0800

    feat:添加构建文件

---
+++ build/precache-manifest.99d59066f690876e3910c05a893251c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8c9c62b7d8aaa5c1d4c92689e8ba4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5baba947e18a848ef26",
+    "url": "/static/css/main.1779617f.chunk.css"
+  },
+  {
+    "revision": "4a7cc3a90e61accc75e8",
+    "url": "/static/js/2.41f10790.chunk.js"
+  },
+  {
+    "revision": "a5baba947e18a848ef26",
+    "url": "/static/js/main.329e87cd.chunk.js"
+  },
+  {
+    "revision": "a9792655675a526cf0de",
+    "url": "/static/js/runtime-main.5f4eecb8.js"
+  }
+]);
