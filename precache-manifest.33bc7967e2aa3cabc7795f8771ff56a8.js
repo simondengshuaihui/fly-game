bdeb64d93fedec2b91a1fcad2c6e807c37ddc22d
simon (from Travis CI) <dengshuaihui@gmail.com>
2020-02-24 13:10:40 +0000

    Deploy simondengshuaihui/fly-game to github.com/simondengshuaihui/fly-game.git:gh-pages

---
+++ precache-manifest.33bc7967e2aa3cabc7795f8771ff56a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3275ae8bbfb101f1b95e462da53256c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "94f750fbe5574c0d57a9",
+    "url": "./static/css/main.1779617f.chunk.css"
+  },
+  {
+    "revision": "97feacf02a188e826794",
+    "url": "./static/js/2.4f1a635b.chunk.js"
+  },
+  {
+    "revision": "94f750fbe5574c0d57a9",
+    "url": "./static/js/main.5984cb34.chunk.js"
+  },
+  {
+    "revision": "6452f30953872a987134",
+    "url": "./static/js/runtime-main.24e7f70f.js"
+  }
+]);
