cb4cb3792cea858c311bb157278e2ea03679f01e
dengshuaihui <dengshuaihui@yy.com>
2020-02-24 20:59:42 +0800

    fix: 打包路径修改

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fly-game/build/precache-manifest.fcda4d2ab5a629d7ae85ce6048181734.js"
+  "./precache-manifest.bf5d189e90a17485b55ff2c14d8f0691.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fly-game/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
